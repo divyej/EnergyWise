8295f7eeae41149eabc68842b77e4e740eb5330d
Divye <djain94314@gmail.com>
2023-07-15 01:30:43 +0530

    made a request to get the temperature of the city

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const express = require('express');
+const axios = require('axios');
+
+require('dotenv').config();
+
 const app = express();
 const port = 3000;
+
+const api=process.env.API;
+
+app.use(express.json());
+
+app.get('/weather', async(req, res) => {
+    try{
+        const response = await axios.get(`https://api.tomorrow.io/v4/timelines?location=40.75872069597532,-73.98529171943665&fields=temperature&timesteps=1h&units=metric&apikey=${api}`);
+    
+    const weatherData = response.data.data.timelines[0].intervals[0].values;
+    res.json(weatherData);
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).send('Some error occured');
+    }
+})
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
